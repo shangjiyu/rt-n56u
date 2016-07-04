34069bf501854cf3ed1e7b80aad106520b9b3f18
shangjiyu <shangjiyu@gmail.com>
2016-07-04 16:33:46 +0800

    add board RY-1A

---
+++ trunk/configs/boards/RY-1A/board.h
@@ -0,0 +1,28 @@
+/* DTeam RY-1A */
+
+#define BOARD_PID		"RY-1A"
+#define BOARD_NAME		"RY-1A"
+#define BOARD_DESC		"DTEAM RY-1A Wireless Router"
+#define BOARD_VENDOR_NAME	"Lenovo Computer Inc."
+#define BOARD_VENDOR_URL	"http://www.newbandeng.com/"
+#define BOARD_MODEL_URL		"http://www.newbandeng.com/forum-80-1.html"
+#define BOARD_BOOT_TIME		20
+#define BOARD_FLASH_TIME	120
+#undef BOARD_GPIO_BTN_RESET	
+#define BOARD_GPIO_BTN_WPS	1
+#undef  BOARD_GPIO_LED_ALL
+#define BOARD_GPIO_LED_WIFI	72
+#define BOARD_GPIO_LED_POWER	38
+#define BOARD_GPIO_LED_LAN	41
+#define BOARD_GPIO_LED_WAN	40
+#define BOARD_GPIO_LED_USB	39
+#undef  BOARD_GPIO_LED_ROUTER
+#undef  BOARD_GPIO_PWR_USB
+#define BOARD_HAS_5G_11AC	0
+#define BOARD_NUM_ANT_5G_TX	0
+#define BOARD_NUM_ANT_5G_RX	0
+#define BOARD_NUM_ANT_2G_TX	2
+#define BOARD_NUM_ANT_2G_RX	2
+#define BOARD_NUM_ETH_LEDS	1
+#define BOARD_HAS_EPHY_L1000	0
+#define BOARD_HAS_EPHY_W1000	0
