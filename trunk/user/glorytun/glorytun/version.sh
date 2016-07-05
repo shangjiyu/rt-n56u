a648264fe0b67ac7481969a04d1a4578197af01f
shangjiyu <shangjiyu@gmail.com>
2016-07-05 19:24:18 +0800

    fix custom package build script

--- trunk/user/glorytun/glorytun/version.sh
+++ trunk/user/glorytun/glorytun/version.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-[ -z "${VERSION}" ] && VERSION=`git describe --tags --match='v[0-9].*' 2>/dev/null` \
+#[ -z "${VERSION}" ] && VERSION=`git describe --tags --match='v[0-9].*' 2>/dev/null` \
                     && VERSION=${VERSION#v}
 
 [ -z "${VERSION}" ] && VERSION=`cat VERSION 2>/dev/null`
